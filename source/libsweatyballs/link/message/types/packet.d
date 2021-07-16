f1d2196006def55fdd4130939bae54f8569900a9
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 23:51:59 +0200

    Updated packet.d

--- source/libsweatyballs/link/message/types/packet.d
+++ source/libsweatyballs/link/message/types/packet.d
@@ -10,10 +10,28 @@ import libsweatyballs.link.message.core : Message;
 */
 public final class DataPacket : Message
 {
+    private string publicKey;
+    private byte[] payload;
+
     /* TODO: Fix this (this is just to get compilation to work) */
-    this()
+    this(string publicKey, byte[] payload)
     {
         super(new byte[1]);
+
+        this.publicKey = publicKey;
+        this.payload = payload;
+    }
+
+    /* TODO: To be honest we should open a session first */
+    public byte[] encode()
+    {
+        /* TODO: Validate that lengt pf `publicKey` and `payload` are not zero */
+
+        /* Encrypt the payload to the `publicKey` */
+        //byte[] encryptedPayload = RSA.encrypt(publicKey, payload);
+
+        /* TODO: */
+        return null;
     }
 
     /* TODO: Fix this (this is just to get compilation to work) */
