f0ee129076af47dff8a60e53d906eb53331db4a6
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 17:15:15 +0200

    Added some Link-s to test with to configuration

--- source/app.d
+++ source/app.d
@@ -3,6 +3,7 @@ module sweatyballs.main;
 import gogga;
 import libsweatyballs.engine.configuration : Config;
 import libsweatyballs.security.identity : Identity;
+import libsweatyballs.link.core : Link;
 import libsweatyballs.engine.core : Engine;
 
 void main()
@@ -17,6 +18,12 @@ void main()
 	/* Create a new Identity for my router */
 	Identity myIdentity = Identity.newIdentity(1024);
 	config.routerIdentity = myIdentity;
+
+	/* Create some Links */
+	Link[] links;
+	links ~= new Link("interface1");
+	links ~= new Link("interface2");
+	config.links = links;
 
 	/* Create a new Engine */
 	Engine engine = new Engine(config);
