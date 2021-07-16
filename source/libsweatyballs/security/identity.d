92a560996155523f9a0c4ab540322c2be46b8e80
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 15:27:48 +0200

    Added base class for cryptography (and module)

---
+++ source/libsweatyballs/security/identity.d
@@ -0,0 +1,16 @@
+module libsweatyballs.security.identity;
+
+import crypto;
+
+/**
+* Identity
+*
+* Description: represents a router's identity
+*/
+public final class Identity
+{
+    this()
+    {
+
+    }
+}
