9066d1058dfe0b87f546dff1ddba13b61b7e3228
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 16:44:03 +0200

    made Switch a thread, added launch method and added basic event loop

--- source/libsweatyballs/zwitch/core.d
+++ source/libsweatyballs/zwitch/core.d
@@ -1,12 +1,36 @@
 module libsweatyballs.zwitch.core;
 
+import core.threading : Thread;
+
 /**
 * Switch
 *
 * Description: TODO
 *
 */
-public final class Switch
+public final class Switch : Thread
 {
-    
+    private Router router;
+
+    this(Router router)
+    {
+        /* Set the thread's worker function */
+        super(&worker);
+
+        this.router = router;
+    }
+
+    private void worker()
+    {
+        /* TODO: Implement me */
+        while(true)
+        {
+
+        }
+    }
+
+    public void launch()
+    {
+        start();
+    }
 }
