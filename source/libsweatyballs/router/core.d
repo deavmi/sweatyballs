c16fd28bf46fc3de64a36d8188b6795e451832e2
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 16:59:15 +0200

    Added mutex for links array and added initialization for all mutexes

--- source/libsweatyballs/router/core.d
+++ source/libsweatyballs/router/core.d
@@ -3,6 +3,7 @@ module libsweatyballs.router.core;
 import libsweatyballs.link.core : Link;
 import libsweatyballs.security.identity : Identity;
 import core.thread : Thread;
+import core.sync.mutex : Mutex;
 
 /**
 * Router
@@ -15,11 +16,23 @@ public final class Router : Thread
     * Links the router can advertise over
     */
     private Link[] links;
+    private Mutex linksMutex;
 
     this(Identity identity, Link[] links)
     {
         /* Set the thread's worker function */
         super(&worker);
+
+        /* Initialize locks */
+        initMutexes();
+    }
+
+    /**
+    * Initializes all the mutexes
+    */
+    private void initMutexes()
+    {
+        linksMutex = new Mute();
     }
 
     private void worker()
@@ -27,7 +40,7 @@ public final class Router : Thread
         /* TODO: Implement me */
         while(true)
         {
-
+            /* TODO: Cycle through each with timeout wait */
         }
     }
 
