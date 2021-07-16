487917cda0830a8809eaa2d894ef3a48eee1f672
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 15:07:13 +0200

    Added Message type, this is the base class for the Message formats (WIP)

---
+++ source/libsweatyballs/link/message.d
@@ -0,0 +1,52 @@
+module libsweatyballs.link.message;
+
+/**
+* Message
+*
+* base class for encoding/decoding messages
+*/
+public class Message
+{
+    /* TODO: Variable for the protocol buffer */
+    private Protokak message;
+    private byte[] messageBytes;
+
+    /**
+    * TODO: Decoder constructor
+    */
+    this(byte[] bytes)
+    {
+        decode(bytes);
+    }
+
+    private void decode(byte[] bytes)
+    {
+        /* TODO: Decode and get the bformat protobug message out and set it */
+    }
+
+    /**
+    * TODO: Encoder constructor
+    */
+    this(ProtoBuf protoKak)
+    {
+        /* TODO: Set the protoKak */
+        encode(protoKak);
+    }
+
+    private void encode(ProtoBuf protoKak)
+    {
+
+    }
+
+    public ProtoKak getMessage()
+    {
+        return message;
+    }
+
+    public byte[] getButes()
+    {
+        return messageBytes;
+    }
+
+
+}
