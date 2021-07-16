12ff74056062a3f27e4704da1964ac2af94bf28e
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 15:26:00 +0200

    Base classes for encoding and decoding different message types

---
+++ source/libsweatyballs/link/message/types/advertisement.d
@@ -0,0 +1,14 @@
+module libsweatyballs.link.message.types.advertisement;
+
+import libsweatyballs.link.message.core : Message;
+
+/**
+* Advertisement
+*
+* Description: This message type represents a router advertisement which contains information
+* about its routes it is aware of
+*/
+public final class Advertisement : Message
+{
+
+}
---
+++ source/libsweatyballs/link/message/types/packet.d
@@ -0,0 +1,14 @@
+module libsweatyballs.link.message.types.packet;
+
+import libsweatyballs.link.message.core : Message;
+
+/**
+* DataPacket
+*
+* Description: This message type represents a data packet that needs to be routed to its
+* eventual destination
+*/
+public final class DataPacket : Message
+{
+
+}
