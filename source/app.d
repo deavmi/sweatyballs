604105ac7f0a16c40d76861d43b7ae089091cbd8
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 16:25:05 +0200

    Added testing code to main() to create a new Config and pass it to a a constructor for creating a new Engine

--- source/app.d
+++ source/app.d
@@ -1,8 +1,25 @@
 module sweatyballs.main;
 
 import std.stdio;
+import libsweatyballs.engine.configuration : Config;
+import libsweatyballs.engine.core : Engine;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	writeln("Welcome to sweatyballs");
+
+	/* TODO: Add command-line parsing here with jcli */
+
+	/* TODO: testing create config */
+	Config config;
+
+	/* Create a new Identity for my router */
+	Identity myIdentity = Identity.newIdentity(1024);
+	config.routerIdentity = myIdentity;
+
+	/* Create a new Engine */
+	Engine engine = new Engine(config);
+
+	/* Start the engine */
+	engine.launch();
 }
