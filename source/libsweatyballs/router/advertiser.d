f94d11d90f07818eacd292417b985ef34c961867
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 23:27:33 +0200

    Get links from router for advertisements

--- source/libsweatyballs/router/advertiser.d
+++ source/libsweatyballs/router/advertiser.d
@@ -1,13 +1,15 @@
 module libsweatyballs.router.advertiser;
 
-import core.thread : Thread;
+import core.thread : Thread, dur;
 import libsweatyballs.router.core : Router;
 import libsweatyballs.link.core : Link;
+import std.socket;
+import gogga;
 
 public final class Advertiser : Thread
 {
     private Router router;
-    
+
     this(Router router)
     {
         /* Set the thread's worker function */
@@ -23,11 +25,13 @@ public final class Advertiser : Thread
         {
             /* TODO: Cycle through each link and advertise on them */
             /* TODO: Fetch links safely */
-            Link[] links;
+            Link[] links = router.getLinks();
             foreach(Link link; links)
             {
                 advertise(link);
             }
+
+            sleep(dur!("seconds")(1));
         }
     }
 
@@ -36,8 +40,21 @@ public final class Advertiser : Thread
         start();
     }
 
+    /**
+    * Send an IPv6 Multicast advertisement via link-local
+    *
+    * The multicast address used is `ff69::1` because the
+    * sex number is cool and I am a 22 year old virgin
+    */
     private void advertise(Link link)
     {
+        /* TODO: Add error handling */
+        Socket socket = new Socket(AddressFamily.INET6, SocketType.DGRAM, ProtocolType.UDP);
+        byte[] buff = [65,66,66,65,65,66,66,65,65,66,66,65];
+        ulong stats = socket.sendTo(buff, parseAddress("ff69::1%"~link.getInterface(), 6666));
+        socket.close();
 
+        import std.conv : to;
+        gprintln("Status"~to!(string)(stats));
     }
 }
