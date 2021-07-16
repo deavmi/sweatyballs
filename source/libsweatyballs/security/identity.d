8d8e59cb928861709071681b7e8c89af0ed171d9
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-16 15:43:17 +0200

    Added RSA key generation function to create a new Identity with the given RSA key pair

--- source/libsweatyballs/security/identity.d
+++ source/libsweatyballs/security/identity.d
@@ -1,6 +1,6 @@
 module libsweatyballs.security.identity;
 
-import crypto.rsa : RSAKeypair;
+import crypto.rsa : RSAKeypair, RSA;
 
 /**
 * Identity
@@ -10,12 +10,60 @@ import crypto.rsa : RSAKeypair;
 public final class Identity
 {
     /**
-    * RSA asymetric keys
+    * Roiter identity
     */
     private RSAKeyPair rsaKeys;
+    private string fingerprint;
 
-    this()
+    this(RSAKeyPair keys)
     {
+        /* ToDO: Validate keys */
+        validateKeys();
 
+        this.rsakeys = keys;
+
+        /* Generate fingerprint */
+        fingerprint = generateFingerprint();
     }
+
+    public static string generateFingerprint(RSAKeyPair keys)
+    {
+        string fingerprint;
+        
+        /* TODO: Return fingerprint */
+        return fingerprint;
+    }
+
+    private void validateKeys(RSAKeyPair keys)
+    {
+        /* TODO: make sure non-empty and that they are related */
+    }
+
+
+    /**
+    * Creates a new router identity. This includes generating a new
+    * set of the following:
+    * 1. An RSA key-pair
+    * 2. TODO
+    *
+    * @param uint rsaBitLength: This is the bit length of the RSA keys
+    */
+    public static Identity newIdentity(uint rsaBitLength)
+    {
+        Identity identity;
+
+        /* Create new RSA keys */
+        RSAKeyPair rsaKeys = RSA.generateKeyPair(rsaBitLength);
+
+        /* TODO: Create a public key fingerprint */
+
+        return identity;
+    }
+}
+
+unittest
+{
+    Identity identity;
+
+    identity = Identity.newIdentity(uint 1024);
 }
