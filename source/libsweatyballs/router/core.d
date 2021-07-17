40bec3f93bf7a4634bad0484e79eb32f8a9d91ba
Tristan B. Kildaire <deavmi@disroot.org>
2021-07-17 18:17:27 +0200

    Fixed compilation error

--- source/libsweatyballs/router/core.d
+++ source/libsweatyballs/router/core.d
@@ -6,7 +6,7 @@ import libsweatyballs.router.table : Table, Route;
 import core.thread : Thread, dur;
 import core.sync.mutex : Mutex;
 import libsweatyballs.router.advertiser : Advertiser;
-import libsweatyballs.link.message.core : Message, test;
+import libsweatyballs.link.message.core : Message;
 import libsweatyballs.engine.core : Engine;
 import gogga;
 import std.conv : to;
