53192362e897bc81dc53ce795fce7b666964775d
Tristan B. Kildaire <deavmi@redxen.eu>
2021-07-20 16:59:41 +0200

    Neighbors can now be compared. They are said to be ewqual when they have matching identity and LInk fields.

--- source/libsweatyballs/zwitch/neighbor.d
+++ source/libsweatyballs/zwitch/neighbor.d
@@ -3,6 +3,7 @@ module libsweatyballs.zwitch.neighbor;
 import std.socket : Address;
 import std.conv : to;
 import libsweatyballs.link.core : Link;
+import std.string : cmp;
 
 public final class Neighbor
 {
@@ -40,5 +41,19 @@ public final class Neighbor
     public override string toString()
     {
         return "NBR ("~identity~") ["~to!(string)(address)~"]";
+    }
+
+    /**
+    * Neighbors are considered equal if both their Link
+    * is the same and their identity. There should not
+    * be several neighbors with same identity on one Link
+    */
+    public override bool opEquals(Object other)
+    {
+        Neighbor otherN = cast(Neighbor)other;
+
+
+
+        return otherN.getLink() == this.getLink() && cmp(otherN.getIdentity(), this.getIdentity()) == 0;
     }
 }
