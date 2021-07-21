9180e36382d8f00789ad03ce634032023eafbc7b
Tristan B. Kildaire <deavmi@redxen.eu>
2021-07-21 11:07:52 +0200

    Added some stuff for Sessions

--- source/libsweatyballs/session/session.d
+++ source/libsweatyballs/session/session.d
@@ -8,4 +8,37 @@ module libsweatyballs.session.session;
 *
 * It is intended one then used Session system for exchanging data in a faster
 * way than directly usign PACKET and LinkMessages
+*
+* It takes in the Engine API and uses that as a way to talk to the network
 */
+public final class SessionManager
+{
+    private Session[] sessions;
+    private Engine engine;
+
+    this(Engine engine)
+    {
+
+    }
+
+    public Session newSession(string address)
+    {
+        
+    }
+}
+
+public final class Session : Thread
+{
+    this(string address)
+    {
+        init(address);
+    }
+
+    private void init(string address)
+    {
+        /* TODO: Generate new AES key */
+        /* TODO: Generate Session MessageType LinkMessage with key in it */
+        /* TODO: Send a packet */
+        /* TODO: Await on Engine for a certain message type */
+    }
+}
